6b2bb393b0871a2192e8881d426c0d2b1d8a5612
gamgam0330 <gamgam333@naver.com>
2019-10-20 20:55:18 +0900

    hihi

--- hello.c
+++ hello.c
@@ -1,12 +1,7 @@
-#include <stdio.h>
+i#include <stdio.h>
 
 int main (void)
 {
-    printf("hello");
-    printf("hello");
-    printf("hello");
-    printf("hello");
-    printf("hello");
     printf("hello");
     printf("hello");
     printf("hello");
